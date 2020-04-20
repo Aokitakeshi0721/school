1ba3931cfcdf5a7c16ea17c837430914d7b57389
Aokitakeshi0721 <yahpan@gmail.com>
2020-04-20 22:21:16 +0900

    delete add

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -39,6 +39,15 @@ class StudentsController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy_all
+    checked_data = params[:deletes].keys # ここでcheckされたデータを受け取っています。
+    if Student.destroy(checked_data)
+      redirect_to root_path
+    else
+      render action: 'index'
+    end
+  end
+
   private
   def student_params
     params.require(:student).permit(:学籍番号, :登録番号, :本国氏名1, :本国氏名2, :フリガナ名, :性別, :未既婚, :国名, :生年月日, :紹介者名, :本国住所書体, :本国住所, :本国電話, :旅券番号, :旅券有効期限, :入国年月日, :生VISA種類, :VISA更新日1, :VISA更新日2, :VISA更新日3, :VISA更新日4, :入管許可番号, :入管許可日, :在留カード番号, :入学日, :卒修予定日, :コース名, :クラス名, :退学年月日, :除籍年月日, :退学除籍理由, :在籍卒業区分, :以前住所〒, :以前住所住, :現住所〒, :現住所住, :現住所電話, :携　帯電話, :国民健康保険番号, :障害保険有無, :母国語, :卒業後進路先, :紹介者書体, :紹介者名1, :紹介者名2, :紹介者住所1, :紹介者住所2, :紹介者TEL, :紹介者FAX, :Jﾃｽﾄ, :NATﾃｽﾄ, :入学ﾃｽﾄ, :組分ﾃｽﾄ)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'students#index'
+  delete :students, to: 'students#destroy_all'
   resources :students do
     collection { post :import }
   end
